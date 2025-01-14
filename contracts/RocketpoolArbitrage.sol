a831bdb95aa1e5971e8de5cca1de3cc7e319a9f4
TrooperCrypto <trooper.crypto7@gmail.com>
2025-01-14 11:09:21 +0100

    update contract
    - allow callers to set the sqrtPriceLimit
    - remove ineffective uniswap auth
    - remove unused constants
    - immutable -> constant
    - approve morpho directly
    - update comments

--- contracts/RocketpoolArbitrage.sol
+++ contracts/RocketpoolArbitrage.sol
@@ -67,32 +67,28 @@ interface IRETH {
 contract RocketpoolExitArbitrage is IUniswapV3SwapCallbackReceiver, IMorphoFlashLoanCallback {
     IWETH public constant WETH = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     IRETH public constant RETH = IRETH(0xae78736Cd615f374D3085123A210448E74Fc6393);
-    address immutable rocketStorage = 0x1d8f8f00cfa6758d7bE78336684788Fb0ee0Fa46;
-    bytes32 constant depositPoolKey = keccak256("contract.addressrocketDepositPool");
-    IMorphoBase immutable Morpho = IMorphoBase(0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb);
-    address immutable Paraswap = 0x6A000F20005980200259B80c5102003040001068;
+    IMorphoBase constant Morpho = IMorphoBase(0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb);
+    address constant Paraswap = 0x6A000F20005980200259B80c5102003040001068;
     
-    uint160 internal constant MIN_SQRT_RATIO = 4295128739;
-    uint160 internal constant MAX_SQRT_RATIO = 1461446703485210103287273052203988822378723970342;
-
-    event Arbitrage(address indexed caller, address indexed receiver, address uniswapPool, uint256 amount, uint256 profit);
+    event Arbitrage(address indexed caller, address indexed receiver, address flashloanProvider, uint256 amount, uint256 profit);
 
     constructor() {}
 
     receive () external payable {}
 
-    /// @notice Withdraws rETH from RocketPool, swaps it for ETH on Uniswap, and sends the profit to the caller.
-    /// @dev The caller must approve this contract to spend rETH.
+    /// @notice Distributes minipools and executes an arbitrage operation using Uniswap V3.
     /// @param _uniswapPool The address of the Uniswap V3 pool to trade rETH for ETH
     /// @param _amount The amount of rETH to withdraw from RocketPool
     /// @param _minProfit The minimum amount of profit to keep, otherwise revert
-    function arb(address _uniswapPool, uint256 _amount, uint256 _minProfit, address _receiver) external {
+    /// @dev This function distributes the minipools, performs a swap using Uniswap V3, and ensures the profit meets the minimum requirement before transferring it to the receiver.
+    /// @dev Emits an {Arbitrage} event.
+    function arb(address _uniswapPool, uint160 _sqrtPriceLimitX96, uint256 _amount, uint256 _minProfit, address _receiver) external {
         IUniswap(_uniswapPool).swap(
             address(this),
             false, // zeroForOne
             int256(_amount),
-            MAX_SQRT_RATIO - 1,
-            abi.encode(_uniswapPool)
+            _sqrtPriceLimitX96,
+            bytes("")
         );
 
         uint256 profit = address(this).balance;
@@ -106,31 +102,35 @@ contract RocketpoolExitArbitrage is IUniswapV3SwapCallbackReceiver, IMorphoFlash
     // see: https://github.com/Uniswap/v3-core/blob/main/contracts/interfaces/callback/IUniswapV3SwapCallback.sol
     /// @notice Called to `msg.sender` after executing a swap via IUniswapV3Pool#swap.
     /// @dev In the implementation you must pay the pool tokens owed for the swap.
-    /// The caller of this method must be checked to be a UniswapV3Pool deployed by the canonical UniswapV3Factory.
-    /// amount0Delta and amount1Delta can both be 0 if no tokens were swapped.
     /// @param _amountRETHDelta The amount of rETH that was sent (negative) or must be received (positive) by the pool by
     /// the end of the swap. If positive, the callback must send that amount of token0 to the pool.
     /// @param _amountWETHDelta The amount of WETH that was sent (negative) or must be received (positive) by the pool by
     /// the end of the swap. If positive, the callback must send that amount of token1 to the pool.
-    /// @param _data Any data passed through by the caller via the IUniswapV3PoolActions#swap call
     function uniswapV3SwapCallback(
         int256 _amountRETHDelta, // token0
         int256 _amountWETHDelta, // token1
-        bytes calldata _data
+        bytes calldata
     ) external override {
-        (address uniswapPool) = abi.decode(_data, (address)); 
-        require(msg.sender == uniswapPool, "uniswapV3SwapCallback: Unauthorized");
-        
+        require(_amountRETHDelta < 0, "rETH must be sent to the pool");
+        require(_amountWETHDelta > 0, "WETH must be received from the pool");
+
         // 1st: burn rETH, receive ETH
         RETH.burn(uint256(-_amountRETHDelta));
 
-        // 2nd: deposit ETH to RocketDepositPool, keep profit in ETH
+        // 2nd: wrap the amount due to the pool, keep the profit in ETH
         WETH.deposit{value: uint256(_amountWETHDelta)}();
 
         // 3rd: Repay the pool to complete the swap
-        WETH.transfer(uniswapPool, uint256(_amountWETHDelta));
+        WETH.transfer(msg.sender, uint256(_amountWETHDelta));
     }
 
+    /// @notice Distributes minipools and executes an arbitrage operation using Paraswap.
+    /// @param _amount The amount of WETH to flash loan.
+    /// @param _data The data required for the flash loan operation.
+    /// @param _minProfit The minimum profit required for the operation to be considered successful.
+    /// @param _receiver The address to receive the profit.
+    /// @dev This function distributes the minipools, performs a flash loan using Morpho, and ensures the profit meets the minimum requirement before transferring it to the receiver.
+    /// @dev Emits an {Arbitrage} event.
     function arbParaswap(uint256 _amount, bytes calldata _data, uint256 _minProfit, address _receiver) external {
         Morpho.flashLoan(address(WETH), _amount, _data);
 
@@ -143,15 +143,18 @@ contract RocketpoolExitArbitrage is IUniswapV3SwapCallbackReceiver, IMorphoFlash
     }
 
     function onMorphoFlashLoan(uint256 amountWethBorrowed, bytes calldata data) external override {
-        WETH.approve(0x6A000F20005980200259B80c5102003040001068, amountWethBorrowed);
+        WETH.approve(Paraswap, amountWethBorrowed);
 
+        // 1st: execute trade, swap WETH for rETH
         (bool success, ) = Paraswap.call(data);
         require(success, "Paraswap failed");
 
+        // 2nd: burn rETH, receive ETH
         uint rethBalance = RETH.balanceOf(address(this));
         RETH.burn(rethBalance);
 
+        // 3rd: wrap the amount due to Morpho, keep the profit in ETH
         WETH.deposit{value: amountWethBorrowed}();
-        WETH.approve(msg.sender, amountWethBorrowed);
+        WETH.approve(address(Morpho), amountWethBorrowed);
     }
 }
--- test/RocketpoolArbitrage.t.sol
+++ test/RocketpoolArbitrage.t.sol
@@ -44,61 +44,71 @@ contract RocketpoolExitArbitrageForkTest is Test {
         // [Optional] Labeling for better console logs
         vm.label(MAINNET_WETH, "WETH");
         vm.label(MAINNET_RETH, "rETH");
-        vm.label(UNISWAP_RETH_WETH_POOL, "rETH/WETH_Pool");
         vm.label(address(rocketpoolExitArb), "RocketpoolExitArb");
         vm.label(0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb, "Morpho");
         vm.label(0x6A000F20005980200259B80c5102003040001068, "Paraswap");
     }
 
     function testArbOnForkUniswap() public {
-        uint256 ethAvailableBeforeArb = MAINNET_RETH.balance;
+        uint256 profitBefore = address(this).balance;
+        uint256 arbBefore = address(rocketpoolExitArb).balance;
+        uint256 rethBefore = MAINNET_RETH.balance;
 
-        // 3) Impersonate a node
+        // Impersonate node
         address nodeAddress = vm.envAddress("NODE_ADDRESS");
         vm.label(nodeAddress, "node address");
         vm.startPrank(nodeAddress);
 
-        // 4) Distribute minipool
-        address minipoolAddress = vm.envAddress("MINIPOOL_ADDRESS");
+        // 1) Distribute minipool
+        address minipoolAddress = 0x0e4aAff61d11a2c36cc29a744885ab73A682EaCF;
         vm.label(minipoolAddress, "minipool address");
-
 
         RPMinipool(minipoolAddress).distributeBalance(false);
 
         vm.stopPrank();
 
-        // 5) Execute the arb
+        address _uniswapPool = 0xa4e0faA58465A2D369aa21B3e42d43374c6F9613;
+        uint160 _sqrtPriceLimitX96 = 84072759829691353131019927552;
+        uint256 _amount = 12346337220937533;
+        uint256 _minProfit = 61816612301843;
+
+        vm.label(_uniswapPool, "UniswapPool");
+
+        // 2) Execute the arb
         rocketpoolExitArb.arb(
-            UNISWAP_RETH_WETH_POOL,
-            23897692000000000000,
-            58322582921635196,
+            _uniswapPool,
+            _sqrtPriceLimitX96,
+            _amount,
+            _minProfit,
             address(this)
         );
 
-        uint256 ethAvailableAfterArb = MAINNET_RETH.balance;
 
-
-        // 6) Validate results or log final balances
-        //    In a real test, you'd check the user’s ETH balance, logs, etc.
         console.log("=== Test completed. Check logs or balances for profit. ===");
 
-        // 7) check arb contract balance
-        console.log("Arb contract balance: ", address(rocketpoolExitArb).balance);
-        console.log("rETH balance before arb: ", ethAvailableBeforeArb);
-        console.log("rETH balance after arb: ", ethAvailableAfterArb);
-        require(ethAvailableBeforeArb <= ethAvailableAfterArb, "rETH balance should never decrease");
-        require(ethAvailableBeforeArb + 25 > ethAvailableAfterArb, "rETH balance should increase by over 25 wei");
+        console.log("Profit balance before arb: ", profitBefore);
+        console.log("Profit balance after arb: ", address(this).balance);
+        require(address(this).balance > profitBefore, "Profit balance should increase");
+        require(address(this).balance - profitBefore >= _minProfit, "Profit balance should increase by over 25 wei");
+        console.log("Arb contract balance before arb: ", arbBefore);
+        console.log("Arb contract balance after arb:: ", address(rocketpoolExitArb).balance);
+        console.log("rETH balance before arb: ", rethBefore);
+        console.log("rETH balance after arb: ", MAINNET_RETH.balance);
+        require(rethBefore <= MAINNET_RETH.balance, "rETH balance should never decrease");
+        require(rethBefore + 25 > MAINNET_RETH.balance, "rETH balance should increase by over 25 wei");
     }
 
     function testArbOnForkParaswap() public {
-        uint256 ethAvailableBeforeArb = MAINNET_RETH.balance;
+        uint256 profitBefore = address(this).balance;
+        uint256 arbBefore = address(rocketpoolExitArb).balance;
+        uint256 rethBefore = MAINNET_RETH.balance;
 
-        // 3) Impersonate a node
+        // Impersonate node
         address nodeAddress = vm.envAddress("NODE_ADDRESS");
         vm.label(nodeAddress, "node address");
         vm.startPrank(nodeAddress);
 
-        // 4) Distribute minipool
+        // 1) Distribute minipool
         address minipoolAddress = vm.envAddress("MINIPOOL_ADDRESS");
         vm.label(minipoolAddress, "minipool address");
 
@@ -107,26 +117,30 @@ contract RocketpoolExitArbitrageForkTest is Test {
 
         vm.stopPrank();
 
-        // 5) Execute the arb
+        bytes memory _swapData = bytes("");
+        uint256 _amount = 12346337220937533;
+        uint256 _minProfit = 61816612301843;
+
+        // 2) Execute the arb
         rocketpoolExitArb.arbParaswap(
-            87145304340033234, 
-            hex"5e94e28d0000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000001e0000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000000000000000000000000ae78736cd615f374d3085123a210448e74fc639300000000000000000000000000000000000000000000000001359a314d07eed200000000000000000000000000000000000000000000000001147faa4996c8ef00000000000000000000000000000000000000000000000001359a314d07eed22d9132db1e4346b99d63a28ed7a96dcf00000000000000000000000001496353000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000060000000000000000000000000ae78736cd615f374d3085123a210448e74fc6393000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc200000000000000000000000000000000000000000000000000000000000000640000000000000000000000000000000000000000000000000000000000000000", 
-            0,
+            _amount, 
+            _swapData, 
+            _minProfit,
             address(this)
         );
 
-        uint256 ethAvailableAfterArb = MAINNET_RETH.balance;
 
-
-        // 6) Validate results or log final balances
-        //    In a real test, you'd check the user’s ETH balance, logs, etc.
         console.log("=== Test completed. Check logs or balances for profit. ===");
 
-        // 7) check arb contract balance
-        console.log("Arb contract balance: ", address(rocketpoolExitArb).balance);
-        console.log("rETH balance before arb: ", ethAvailableBeforeArb);
-        console.log("rETH balance after arb: ", ethAvailableAfterArb);
-        require(ethAvailableBeforeArb <= ethAvailableAfterArb, "rETH balance should never decrease");
-        require(ethAvailableBeforeArb + 25 > ethAvailableAfterArb, "rETH balance should increase by over 25 wei");
+        console.log("Profit balance before arb: ", profitBefore);
+        console.log("Profit balance after arb: ", address(this).balance);
+        require(address(this).balance > profitBefore, "Profit balance should increase");
+        require(address(this).balance - profitBefore > 25, "Profit balance should increase by over 25 wei");
+        console.log("Arb contract balance before arb: ", arbBefore);
+        console.log("Arb contract balance after arb:: ", address(rocketpoolExitArb).balance);
+        console.log("rETH balance before arb: ", rethBefore);
+        console.log("rETH balance after arb: ", MAINNET_RETH.balance);
+        require(rethBefore <= MAINNET_RETH.balance, "rETH balance should never decrease");
+        require(rethBefore + 25 > MAINNET_RETH.balance, "rETH balance should increase by over 25 wei");
     }
 }
